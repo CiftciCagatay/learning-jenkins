ebffc643669fb4c92cde9d50acee45a3027cc2e3
Cagatay Ciftci <im.cgtycftc@gmail.com>
2019-06-29 16:21:11 +0300

    Fix typo

--- jenkins/dockerize_and_push.sh
+++ jenkins/dockerize_and_push.sh
@@ -2,4 +2,4 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 docker build --no-cache -t bluefx/learning-jenkins .
 
-docker push bluefx/learning/jenkins
+docker push bluefx/learning-jenkins
